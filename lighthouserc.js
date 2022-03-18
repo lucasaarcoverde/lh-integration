faad1bab5f99fca90750e82a9d113db42fc6f9f5
Lucas Arcoverde <lucasaarcoverde@gmail.com>
2022-03-18 16:04:47 -0300

    Admin UI 116.x

--- lighthouserc.js
+++ lighthouserc.js
@@ -10,8 +10,13 @@ module.exports = {
       serverBaseUrl: "https://gentle-lowlands-17553.herokuapp.com",
       token: "9fdfb396-c6fb-4547-bc85-8be4b01db776",
     },
-    assert: {
-      preset: "lighthouse:recommended",
+    assertions: {
+      "categories:performance": ["warn", { minScore: 0.8 }],
+      "categories:accessibility": ["warn", { minScore: 0.8 }],
+      "categories:best-practices": ["warn", { minScore: 0.8 }],
+    },
+    settings: {
+      onlyCategories: ["performance", "best-practices", "accessibility"],
     },
   },
 };
